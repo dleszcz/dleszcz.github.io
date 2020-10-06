95de088e1ebd187d1f953144fa31b66fb63f606f
Daniel Leszczyński <danielxleszczynski@gmail.com>
2020-10-06 09:41:50 +0200

    Updates

---
+++ precache-manifest.67bf4202637645b30dfbe44962e936df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3280c4cee2505cb39e8c08b663022684",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31965e4ba1f73af62e18",
+    "url": "/static/css/main.754f0dae.chunk.css"
+  },
+  {
+    "revision": "27b1e9e34b0f0df34be2",
+    "url": "/static/js/2.4203de24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4203de24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31965e4ba1f73af62e18",
+    "url": "/static/js/main.610ed5d4.chunk.js"
+  },
+  {
+    "revision": "ba08d51e29aace5db122",
+    "url": "/static/js/runtime-main.ff3de556.js"
+  }
+]);
